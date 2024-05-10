8f11865eb43c4c387ed737bab7be27e218fb564c
HelloAldis <307665930@qq.com>
2024-05-10 23:30:15 +0800

    add camila blog

--- src/navigation.js
+++ src/navigation.js
@@ -34,14 +34,15 @@ export const footerData = {
         { text: 'AI', href: '#' },
         { text: 'Web3', href: '/aldis/category/web3' },
         { text: '元宇宙', href: '#' },
-        { text: '游戏', href: '#' },
+        { text: '硬件', href: '#' },
+        { text: '编程随感', href: '/aldis/category/bian1-cheng2-sui2-gan3' },
       ],
     },
     {
       title: 'Camila',
       links: [
         { text: '动物', href: '#' },
-        { text: '植物', href: '#' },
+        { text: '植物', href: '/camila/category/zhi2-wu4' },
         { text: '风景', href: '#' },
       ],
     },
