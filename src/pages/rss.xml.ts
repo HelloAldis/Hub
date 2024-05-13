435b5571e6ff218c52d8807340a9cb2137e1440b
HelloAldis <307665930@qq.com>
2024-05-13 23:48:36 +0800

    update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -12,19 +12,36 @@ export const GET = async () => {
     });
   }
 
-  const posts = await fetchPosts('aldis');
-
-  const rss = await getRssString({
-    title: `${SITE.name}’s Blog`,
-    description: METADATA?.description || '',
-    site: import.meta.env.SITE,
-
-    items: posts.map((post) => ({
+  const aldisPosts = await fetchPosts('aldis');
+  const aldisItems = aldisPosts.map((post) => ({
       link: getBlogPostPermalink(post.permalink, 'aldis'),
       title: post.title,
       description: post.excerpt,
       pubDate: post.publishDate,
-    })),
+    }));
+
+  const camilaPosts = await fetchPosts('camila');
+  const camilaItems = camilaPosts.map((post) => ({
+      link: getBlogPostPermalink(post.permalink, 'camila'),
+      title: post.title,
+      description: post.excerpt,
+      pubDate: post.publishDate,
+    }));
+
+  const sevenPosts = await fetchPosts('seven');
+  const sevenItems = sevenPosts.map((post) => ({
+      link: getBlogPostPermalink(post.permalink, 'seven'),
+      title: post.title,
+      description: post.excerpt,
+      pubDate: post.publishDate,
+    }));
+
+  const rss = await getRssString({
+    title: `${SITE.name}`,
+    description: METADATA?.description || '',
+    site: import.meta.env.SITE,
+
+    items: [...aldisItems, ...camilaItems, ...sevenItems],
 
     trailingSlash: SITE.trailingSlash,
   });
